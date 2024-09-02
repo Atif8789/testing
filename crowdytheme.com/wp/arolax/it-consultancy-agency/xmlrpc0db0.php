5e50c321b09ae8d0cf18eb2b6bb34c69416e99ae
Atif8789 <atif.equbal@trantorinc.com>
2024-09-02 16:29:36 +0530

    changes 1

---
+++ crowdytheme.com/wp/arolax/it-consultancy-agency/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://crowdytheme.com/wp/arolax/it-consultancy-agency</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://crowdytheme.com/wp/arolax/it-consultancy-agency/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://crowdytheme.com/wp/arolax/it-consultancy-agency/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://crowdytheme.com/wp/arolax/it-consultancy-agency/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://crowdytheme.com/wp/arolax/it-consultancy-agency/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://crowdytheme.com/wp/arolax/it-consultancy-agency/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
